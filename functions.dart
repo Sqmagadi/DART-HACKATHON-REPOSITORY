c85f110fa6e4ed80874380ca0d768a67f832dfdd
SQCodes <paulsqmagadi@gmail.com>
2024-03-16 16:13:28 +0300

    modified:   controlflow.dart 	modified:   functions.dart 	modified:   program1..dart

--- functions.dart
+++ functions.dart
@@ -1,2 +1,23 @@
-## Program 2: Perform Mathematical Operations with Functions
-Write a Dart program that performs two mathematical operations using functions.
+// ## Program 2: Perform Mathematical Operations with Functions
+// Write a Dart program that performs two mathematical operations using functions.
+
+
+void main() {
+  // Perform addition
+  int resultAddition = addition(5, 3);
+  print("Result of addition: $resultAddition");
+
+  // Perform multiplication
+  int resultMultiplication = multiplication(5, 3);
+  print("Result of multiplication: $resultMultiplication");
+}
+
+// Function to perform addition
+int addition(int a, int b) {
+  return a + b;
+}
+
+// Function to perform multiplication
+int multiplication(int a, int b) {
+  return a * b;
+}
--- program1..dart
+++ program1..dart
@@ -1,2 +1,14 @@
-## Program 1: Display Personal Information
-Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+// ## Program 1: Display Personal Information
+// Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+
+
+void main() {
+  // Define variables for personal information
+  String name = "Paul";
+  int age = 25;
+  String school = "PLP School";
+  String hobby = "Coding";
+
+  // Print personal information in one logical sentence
+  print("My name is $name. I am $age years old. I study at $school, and my hobby is $hobby.");
+}
